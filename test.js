0417b8c5df9015bef9deb4f4f81faf1df7c23150
Changbai Li <changbai.li@heliosinteractive.com>
2017-04-25 15:48:56 -0700

    add non-automated test

---
+++ test.js
@@ -0,0 +1,7 @@
+// TODO use test runner to automate this test
+
+const findBandName = require('./lib/findBandName');
+
+const text = 'This is a very good news, everyone!';
+
+console.log(findBandName(text)); // should log [ 'Very Good News' ]

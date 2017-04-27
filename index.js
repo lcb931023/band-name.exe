93e14d6053348d19e5b5f17834c2fc89a7a91659
Changbai Li <changbai.li@heliosinteractive.com>
2017-04-27 14:46:04 -0700

    install botkit and example project

---
+++ index.js
@@ -0,0 +1,98 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+
+This is a sample Slack bot built with Botkit.
+
+This bot demonstrates a multi-stage conversation
+
+# RUN THE BOT:
+
+  Get a Bot token from Slack:
+
+    -> http://my.slack.com/services/new/bot
+
+  Run your bot from the command line:
+
+    SLACK_BOT_TOKEN=<MY TOKEN> node demo_bot.js
+
+# USE THE BOT:
+
+  Find your bot inside Slack
+
+  Say: "pizzatime"
+
+  The bot will reply "What flavor of pizza do you want?"
+
+  Say what flavor you want.
+
+  The bot will reply "Awesome" "What size do you want?"
+
+  Say what size you want.
+
+  The bot will reply "Ok." "So where do you want it delivered?"
+
+  Say where you want it delivered.
+
+  The bot will reply "Ok! Goodbye."
+
+  ...and will refrain from billing your card because this is just a demo :P
+
+# EXTEND THE BOT:
+
+  Botkit has many features for building cool and useful bots!
+
+  Read all about it here:
+
+    -> http://howdy.ai/botkit
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var Botkit = require('botkit');
+
+if (!process.env.SLACK_BOT_TOKEN) {
+  console.log('Error: Specify SLACK_BOT_TOKEN in environment');
+  process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+ debug: false
+});
+
+console.log(process.env.SLACK_BOT_TOKEN);
+controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN
+}).startRTM(function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+controller.hears(['pizzatime'],['ambient','direct_message'],function(bot,message) {
+  bot.startConversation(message, askFlavor);
+});
+
+askFlavor = function(response, convo) {
+  convo.ask("What flavor of pizza do you want?", function(response, convo) {
+    convo.say("Awesome.");
+    askSize(response, convo);
+    convo.next();
+  });
+}
+askSize = function(response, convo) {
+  convo.ask("What size do you want?", function(response, convo) {
+    convo.say("Ok.")
+    askWhereDeliver(response, convo);
+    convo.next();
+  });
+}
+askWhereDeliver = function(response, convo) {
+  convo.ask("So where do you want it delivered?", function(response, convo) {
+    convo.say("Ok! Goodbye.");
+    convo.next();
+  });
+}
